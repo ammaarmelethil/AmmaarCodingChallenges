037e0fb06dfdbcb56c409914b2e813814c50277d
ammaar <ammaar@ammaars-air.cg.shawcable.net>
2022-10-16 17:18:24 -0600

    Created method smallestDigit
    Finds the smallest digit in a given int argument.
    
    Passed respective JUnit tests.
    
    All JUnit tests for this CC pass now.

--- src/basicjava/CCStringsIfAndWhile.java
+++ src/basicjava/CCStringsIfAndWhile.java
@@ -35,7 +35,30 @@ public class CCStringsIfAndWhile {
 
 	public static int smallestDigit(int i) {
 		
-		return 0;
+		// initializes smallest number to 10 so condition in while loop can hold true for digits
+		int smallestNumber = 10;
+		int lastDigit = 0;
+		
+		
+		// checks and/or corrects argument if i<0 or i == 0
+		if (i < 0) {
+			i *= -1;
+		} else if (i==0) {
+			smallestNumber = i;
+		}
+		
+		// iterates through each digit and resets smallestNumber if condition holds true
+		while (i > 0) {
+			lastDigit = i % 10;
+			
+			if (lastDigit < smallestNumber) {
+				smallestNumber = lastDigit;
+			}
+			
+			i /= 10;
+		}
+		
+		return smallestNumber;
 	}
 
 }
