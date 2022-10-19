6b24b0118e2930b8b10330a735b871f38145addd
ammaar <ammaar@ammaars-air.cg.shawcable.net>
2022-10-19 16:40:58 -0600

    Created indexOfIgnore case method
    - finds index of provided String in provided ArrayList
    - Case insensitive

--- src/basicjava/CCArrayList.java
+++ src/basicjava/CCArrayList.java
@@ -4,14 +4,26 @@ import java.util.ArrayList;
 
 public class CCArrayList {
 
-	public static int indexOfIgnoreCase(ArrayList<String> strs, String string) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public static void insert(ArrayList<Double> nums, double d, int i) {
-		// TODO Auto-generated method stub
+	public static int indexOfIgnoreCase(ArrayList<String> strs, String toFind) {
+		
+		int indexOfStringtoFind = -1;
+		
+		for (int i = 0; i < strs.size(); i++) {
+			if (toFind.equalsIgnoreCase(strs.get(i))) {
+				indexOfStringtoFind = i;
+				i = strs.size();
+				
+			} 
+		}
+		
+		return indexOfStringtoFind;
 		
 	}
+
+	public static void insert(ArrayList<Double> nums, double numToInsert, int insertAtIndex) {
+		
+		
+	}
+	
 
 }
