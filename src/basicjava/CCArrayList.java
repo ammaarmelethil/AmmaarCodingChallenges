c232be7ea52b21e7bded43e27fdb962af73440b6
ammaar <ammaar@ammaars-air.cg.shawcable.net>
2022-10-19 15:34:23 -0600

    Created skeleton class for CC ArrayLists.

---
+++ src/basicjava/CCArrayList.java
@@ -0,0 +1,17 @@
+package basicjava;
+
+import java.util.ArrayList;
+
+public class CCArrayList {
+
+	public static int indexOfIgnoreCase(ArrayList<String> strs, String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void insert(ArrayList<Double> nums, double d, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
