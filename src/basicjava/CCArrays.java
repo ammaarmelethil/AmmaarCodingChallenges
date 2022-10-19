648f9e717f0787d057c208be192c79f9cca2eb8d
ammaar <ammaar@ammaars-air.cg.shawcable.net>
2022-10-19 15:19:15 -0600

    Created void replace method
    - Replaces specified values in a char Array with other specified values.
    
    - Is case insensitive.

--- src/basicjava/CCArrays.java
+++ src/basicjava/CCArrays.java
@@ -1,15 +1,21 @@
 package basicjava;
 
+
 public class CCArrays {
 
 	public static void replace(char[] charArray, char toReplace, char replaceWith) {
-		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < charArray.length; i++) {
+			if (Character.toLowerCase(charArray[i]) == Character.toLowerCase(toReplace)) {
+				charArray[i] = replaceWith;
+			}
+		}
 		
 	}
 
 	public static void sortAlphabetic(String[] strArray) {
-		// TODO Auto-generated method stub
 		
 	}
+	
 
 }
