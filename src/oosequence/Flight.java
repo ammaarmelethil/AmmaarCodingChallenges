8d91fb519a662ba8d1ee29d267cc41bf5c8dbaed
ammaar <ammaar@localhost>
2022-10-31 20:42:38 -0600

    Created constructor for Flight class which accounts for invalid args.

--- src/oosequence/Flight.java
+++ src/oosequence/Flight.java
@@ -1,26 +1,37 @@
 package oosequence;
 
 import java.util.Date;
+//import java.util.concurrent.TimeUnit;
 
 public class Flight {
-	Date arrival;
 	Date departure;
+	Date arrival;
 
 	public Flight(Date departureDate, Date arrivalDate) {
-		// TODO Auto-generated constructor stub
-		departure = departureDate;
-		arrival = arrivalDate;
-	
-	
+		if (departureDate != null && arrivalDate != null) {
+			if (!departureDate.before(arrivalDate)) {
+				departure = null;
+				arrival = null;
+			} else {
+				departure = departureDate;
+				arrival = arrivalDate;
+			}
+			
+		}
+		
 	}
 
-	public Flight(Flight p) {
-		// TODO Auto-generated constructor stub
+	public Flight(Flight toCopy) {
+//		departure = toCopy.departure;
+//		arrival = toCopy.arrival;
 	}
 
 	public long length() {
-		// TODO Auto-generated method stub
-		return 0;
+		long duration = 0;
+//		if (departure != null && arrival != null) {
+//			duration = TimeUnit.MINUTES.convert(arrival.getTime()-departure.getTime(), TimeUnit.MILLISECONDS);
+//		}
+		return duration;
 	}
 
 }
